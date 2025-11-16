17de8a449a4bf2fed016816a77f4cafee15f40f9
marsh31 <marsh22h53@gmail.com>
2025-11-16 23:42:03 +0900

    fix: can bcm code

--- can_demo_bcm_send.c
+++ can_demo_bcm_send.c
@@ -4,6 +4,7 @@
 #include <linux/can/raw.h>
 #include <linux/sockios.h>
 #include <net/if.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +12,77 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+int setup_tx(int s,
+             int ifindex,
+             canid_t can_id,
+             unsigned int period_ms,
+             unsigned int len,
+             uint8_t base_val)
+{
+  struct {
+    struct bcm_msg_head msg_head;
+    struct canfd_frame frame[1];
+  } msg;
+  memset(&msg, 0, sizeof(msg));
+
+  msg.msg_head.opcode  = TX_SETUP;
+  msg.msg_head.flags   = SETTIMER | STARTTIMER | CAN_FD_FRAME;
+  msg.msg_head.can_id  = can_id;
+  msg.msg_head.nframes = 1;
+  msg.msg_head.count   = 0;  // 0 = 無限送信
+
+  msg.msg_head.ival1.tv_sec  = 0;  // 最初の送信までの遅延時間
+  msg.msg_head.ival1.tv_usec = 0;  // 最初の送信までの遅延時間
+
+  msg.msg_head.ival2.tv_sec  = period_ms / 1000;           // 送信周期
+  msg.msg_head.ival2.tv_usec = (period_ms % 1000) * 1000;  // 送信周期
+
+  msg.frame[0].can_id = can_id;
+  msg.frame[0].len    = len;
+  msg.frame[0].flags  = CANFD_BRS;
+  for (int i = 0; i < msg.frame[0].len; i++) {
+    msg.frame[0].data[i] = (unsigned char)i;
+  }
+
+  ssize_t nbytes = send(s, &msg, sizeof(msg), 0);
+  if (nbytes < 0) {
+    perror("Write TX_SETUP");
+    close(s);
+    return -1;
+  }
+
+  return 0;
+}
+
+
+int update_fd_data(int s, canid_t can_id, __u8 *new_data, __u8 len)
+{
+  struct {
+    struct bcm_msg_head msg_head;
+    struct canfd_frame frame[1];
+  } upd;
+  memset(&upd, 0, sizeof(upd));
+
+  upd.msg_head.opcode  = TX_SETUP;
+  upd.msg_head.flags   = CAN_FD_FRAME;
+  upd.msg_head.can_id  = can_id;
+  upd.msg_head.nframes = 1;
+
+  upd.frame[0].can_id = can_id;
+  upd.frame[0].len    = len;
+  upd.frame[0].flags  = CANFD_BRS;
+
+  memcpy(upd.frame[0].data, new_data, len);
+
+  ssize_t nbytes = send(s, &upd, sizeof(upd), 0);
+  if (nbytes < 0) {
+    perror("TX_SETUP(update)");
+    return -1;
+  }
+
+  return 0;
+}
 
 int main()
 {
@@ -42,41 +114,12 @@ int main()
     return 1;
   }
 
-  struct {
-    struct bcm_msg_head msg_head;
-    struct canfd_frame frame[2];
-  } msg;
-  memset(&msg, 0, sizeof(msg));
-
-  msg.msg_head.opcode  = TX_SETUP;
-  msg.msg_head.flags   = SETTIMER | STARTTIMER | CAN_FD_FRAME;
-  msg.msg_head.can_id  = 0x123;
-  msg.msg_head.nframes = 1;
-  msg.msg_head.count   = 0;  // 0 = 無限送信
-
-  msg.msg_head.ival1.tv_sec  = 1;  // 最初の送信までの遅延時間
-  msg.msg_head.ival1.tv_usec = 0;  // 最初の送信までの遅延時間
-
-  msg.msg_head.ival2.tv_sec  = 1;  // 送信周期
-  msg.msg_head.ival2.tv_usec = 0;  // 送信周期
-
-  msg.frame[0].can_id = 0x0123;
-  msg.frame[0].len    = 8;
-  msg.frame[0].flags  = CANFD_BRS;
-  for (int i = 0; i < msg.frame[0].len; i++) {
-    msg.frame[0].data[i] = (unsigned char)i;
+  if (setup_tx(s, ifr.ifr_ifindex, 0x123, 1000, 8, 0x00) < 0) {
+    close(s);
+    return 1;
   }
 
-  msg.frame[1].can_id = 0x0124;
-  msg.frame[1].len    = 4;
-  msg.frame[1].flags  = CANFD_BRS;
-  for (int i = 0; i < msg.frame[1].len; i++) {
-    msg.frame[1].data[i] = (unsigned char)i;
-  }
-
-  ssize_t nbytes = send(s, &msg, sizeof(msg), 0);
-  if (nbytes < 0) {
-    perror("Write TX_SETUP");
+  if (setup_tx(s, ifr.ifr_ifindex, 0x456, 500, 16, 0x00) < 0) {
     close(s);
     return 1;
   }
@@ -86,20 +129,38 @@ int main()
   printf("candump vcan0 などで確認できます。\n");
   printf("終了するには Enter キーを押してください。\n");
 
+
+  int cnt = 0;
+  while (1) {
+    usleep(100000);
+
+    cnt++;
+    __u8 new_payload[8] = {cnt, cnt, 0, 0, cnt, cnt, 0, 0};
+    update_fd_data(s, 0x123, new_payload, 8);
+
+    if (cnt > 1000000) {
+      cnt = 0;
+    }
+  }
+
   // ユーザ入力待ち（その間もカーネルが送信を続ける）
   getchar();
 
   struct bcm_msg_head del;
   memset(&del, 0, sizeof(del));
+
   del.opcode = TX_DELETE;
   del.can_id = 0x123;
+  if (send(s, &del, sizeof(del), 0) < 0) {
+    perror("tx delete 0x123");
+  }
 
-  nbytes = write(s, &del, sizeof(del));
-  if (nbytes < 0) {
-    perror("write TX_DELETE");
-    // 終了はする
-  } else {
-    printf("TX_DELETE sent. Periodic transmission stopped.\n");
+  memset(&del, 0, sizeof(del));
+
+  del.opcode = TX_DELETE;
+  del.can_id = 0x456;
+  if (send(s, &del, sizeof(del), 0) < 0) {
+    perror("tx delete 0x456");
   }
 
   close(s);
